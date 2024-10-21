fa6b31e9009ed8205f09cc1914de1f6384ca3272
Flarkinater <flarkinater@gmail.com>
2024-10-21 19:30:09 -0400

    Basic setup and configs

---
+++ LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace CodeYou_QA_Final {
+    internal class LoginPage {
+        private IWebDriver _driver;
+
+        public LoginPage(IWebDriver d) {
+            _driver = d;
+        }
+
+        public string loginPageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    }
+}
---
+++ UnitTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace CodeYou_QA_Final {
+    [TestClass]
+    public class UnitTests {
+        private IWebDriver _driver;
+        private LoginPage _loginPage;
+
+        [TestInitialize]
+        public void Setup() {
+            _driver = new FirefoxDriver();
+            _loginPage = new LoginPage(_driver);
+        }
+
+        [TestMethod]
+        public void TestMethod1() {
+            _driver.Navigate().GoToUrl(_loginPage.loginPageUrl);
+        }
+
+        [TestCleanup]
+        public void Cleanup() {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
