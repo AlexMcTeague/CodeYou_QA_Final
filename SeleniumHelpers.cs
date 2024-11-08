d0b307a5856b1768ae0cdca096f49d9a63c1ae30
Flarkinater <flarkinater@gmail.com>
2024-11-07 19:27:32 -0500

    Add password generator

--- SeleniumHelpers.cs
+++ SeleniumHelpers.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Linq;
 
 namespace CodeYou_QA_Final {
     internal class SeleniumHelpers {
@@ -9,6 +10,26 @@ namespace CodeYou_QA_Final {
 
         public SeleniumHelpers(IWebDriver driver) {
             _driver = driver;
+        }
+
+        public string GetRandomPassword(int length) {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+0123456789";
+            string nums = "0123456789";
+            var random = new Random();
+
+            string password = new string(Enumerable.Repeat(chars, length - 1)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            char nextChar;
+            if (password.Any(char.IsDigit)) {
+                nextChar = chars[random.Next(chars.Length)];
+            } else {
+                nextChar = nums[random.Next(nums.Length)];
+            }
+
+            password += nextChar;
+
+            return password;
         }
     }
 }
