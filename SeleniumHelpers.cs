dcd4811b5ef18bb7c1c492a2356a5a48234c431b
Flarkinater <flarkinater@gmail.com>
2024-11-09 18:37:12 -0500

    Add Pig Latin helper

--- SeleniumHelpers.cs
+++ SeleniumHelpers.cs
@@ -31,5 +31,34 @@ namespace CodeYou_QA_Final {
 
             return password;
         }
+
+        public string ToPigLatin(string sentence) {
+
+            string vowels = "AEIOUaeiou";
+            List<string> pigWords = new List<string>();
+
+            foreach (string word in sentence.Split(' ')) {
+                string firstLetter = word.Substring(0, 1);
+                string secondLetter = word.Substring(1, 1);
+                string thirdLetter = word.Substring(2, 1);
+                string restOfWord = word.Substring(3, word.Length - 3);
+
+                if (vowels.IndexOf(firstLetter) == -1) {
+                    vowels += "y";
+                    if (vowels.IndexOf(secondLetter) == -1) {
+                        if (vowels.IndexOf(secondLetter) == -1) {
+                            pigWords.Add(restOfWord + firstLetter + secondLetter + thirdLetter + "ay");
+                        } else {
+                            pigWords.Add(thirdLetter + restOfWord + firstLetter + secondLetter + "ay");
+                        }
+                    } else {
+                        pigWords.Add(secondLetter + thirdLetter + restOfWord + firstLetter + "ay");
+                    }
+                } else {
+                    pigWords.Add(word + "yay");
+                }
+            }
+            return string.Join(" ", pigWords);
+        }
     }
 }
